644681205cac6552609b08b589bd1c895d918f7a
SurendharBalakrishnan <surendharal65@gmail.com>
2023-02-26 21:14:47 +0530

    second commit

--- Quiz/app/Models/User.php
+++ Quiz/app/Models/User.php
@@ -20,7 +20,13 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password', 
+        'visible_password',
+        'occupation',
+        'address',
+        'phone',
+        'bio',
+        'is_admin',
     ];
 
     /**
